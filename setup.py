9132c8b7695417352d59da9979c207cd067568d6
darius BERNARD <darius.bernard@onysos.fr>
2013-08-21 15:07:31 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import os
+
+
+setup(name='django-nginx-config',
+      version='0.1',
+      description='an easy to use nginx config generator for django project',
+      author='Darius BERNARD',
+      author_email='contact@onysos.fr',
+      url='https://github.com/onysos/django-nginx-config',
+      download_url='https://github.com/onysos/django-nginx-config/archive/master.zip',
+      package_dir={'django_nginx': 'django_nginx'},
+      packages=["django_nginx"],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Utilities'],
+      )
