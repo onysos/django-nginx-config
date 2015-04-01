bf000d614c6612306d9bd76aec0a6ef668c4c4f4
darius BERNARD <darius.bernard@onysos.fr>
2015-04-01 09:08:21 +0200

    correction du fichie setup.py. (basé sur celui de django-extensions)

--- setup.py
+++ setup.py
@@ -1,6 +1,48 @@
+"""
+Based entirely on Django's own ``setup.py``.
+"""
 from distutils.core import setup
 import os
 
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, package_data = [], {}
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk("django_nginx"):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames:
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
 
 setup(name='django-nginx-config',
       version='0.2',
@@ -10,7 +52,8 @@ setup(name='django-nginx-config',
       url='https://github.com/onysos/django-nginx-config',
       download_url='https://github.com/onysos/django-nginx-config/archive/master.zip',
       package_dir={'django_nginx': 'django_nginx'},
-      packages=["django_nginx"],
+      packages=packages,
+      package_data=package_data,
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
